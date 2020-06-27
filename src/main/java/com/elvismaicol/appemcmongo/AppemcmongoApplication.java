008b7f85716c05f700679905698022477fd71400
Elvis Maicol <elvismaicol@gmail.com>
2020-06-27 17:45:12 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/elvismaicol/appemcmongo/AppemcmongoApplication.java
@@ -0,0 +1,13 @@
+package com.elvismaicol.appemcmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppemcmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppemcmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elvismaicol/appemcmongo/AppemcmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvismaicol.appemcmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppemcmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
